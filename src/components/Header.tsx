ade258f1c5b8f5b718c689fc3028d50eee29ca18
Zacharie Boisnard <mecazack@gmail.com>
2023-04-25 23:47:49 +0200

    fix: import

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import { Link, PageProps } from 'gatsby';
+import { Link } from 'gatsby';
 import { flexCenter, fontMonserrat } from '../style-utils/styles-variables';
 
 const HeaderContainer = styled.header`
