e304b10413d18b9205db58c9bb44660e1f44258f
Zacharie Boisnard <mecazack@gmail.com>
2023-04-28 18:37:10 +0200

    feat: support image

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,61 +1,75 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `Template-ZB`,
-    description: 'Description of site',
-    author: 'Zacharie Boisnard',
-    siteUrl: `https://www.yourdomain.tld`,
-    lang: 'en',
-    menuLinks: [
-      {
-        name: 'Home',
-        link: '/'
-      },
-      {
-        name: 'Test Page in MD',
-        link: '/test-page-in-md/'
-      }
-    ]
-  },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-transformer-remark", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        "name": "images",
-        "path": "./src/images/"
-      },
-      __key: "images"
-    }, {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        "name": "pages",
-        "path": "./src/pages/"
-      },
-      __key: "pages"
-    }, {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        "name": "post",
-        "path": "./src/content"
-      }
-    }, {
-      resolve: `gatsby-plugin-google-fonts`,
-      options: {
-        fonts: [
-          `Montserrat:300,400,500,700` // you can also specify font weights and styles
+    siteMetadata: {
+        title: `Template-ZB`,
+        description: 'Description of site',
+        author: 'Zacharie Boisnard',
+        siteUrl: `https://www.yourdomain.tld`,
+        lang: 'en',
+        menuLinks: [
+            {
+                name: 'Home',
+                link: '/',
+            },
+            {
+                name: 'Test Page in MD',
+                link: '/test-page-in-md/',
+            },
         ],
-        display: 'swap'
-      }
-    }]
+    },
+    // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+    // If you use VSCode you can also use the GraphQL plugin
+    // Learn more at: https://gatsby.dev/graphql-typegen
+    graphqlTypegen: true,
+    plugins: [
+        'gatsby-plugin-styled-components',
+        'gatsby-plugin-image',
+        `gatsby-plugin-sharp`,
+        `gatsby-transformer-sharp`,
+        'gatsby-plugin-sitemap',
+        {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                icon: 'src/images/icon.png',
+            },
+        },
+        'gatsby-transformer-remark',
+        'gatsby-plugin-sharp',
+        'gatsby-transformer-sharp',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'images',
+                path: './src/images/',
+            },
+            __key: 'images',
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'pages',
+                path: './src/pages/',
+            },
+            __key: 'pages',
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'post',
+                path: './src/content',
+            },
+        },
+        {
+            resolve: `gatsby-plugin-google-fonts`,
+            options: {
+                fonts: [
+                    `Montserrat:300,400,500,700`, // you can also specify font weights and styles
+                ],
+                display: 'swap',
+            },
+        },
+    ],
 };
 
 export default config;
